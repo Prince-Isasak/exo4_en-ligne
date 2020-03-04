eefd310b113c558ad4239632a7902f82d61e2e9c
Prince@ <61355057+Prince-Isasak@users.noreply.github.com>
2020-03-04 00:13:12 +0000

    my fourh commit

---
+++ exo4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var NombreMaqique = new Random().Next(3, 10);
+                var NombreDevie = 4;
+
+                while (true)
+                {
+                    
+                   
+                    Console.WriteLine("entrer un nombre");
+                    var Uitilsateur = Console.ReadLine();
+                    var user = Convert.ToInt32(Uitilsateur);
+
+
+                    if (user < NombreMaqique || user > NombreMaqique)
+                    {
+                        Console.WriteLine("veuillez entrer un nombre svp!");
+
+                    }
+                    else
+                    {
+                        if (NombreDevie == 0)
+                        {
+                            Console.WriteLine("vous avez perdu");
+                        }
+
+                        if (user > NombreMaqique)
+                        {
+                            Console.WriteLine("Ce nombre est trop grand");
+                        }
+                        else if (user < NombreMaqique)
+                        {
+                            Console.WriteLine("Ce nombre est trop petit");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bravo vous avez trouvé le nombre magique");
+                            break;
+                        }
+                    }
+                    
+
+                    
+                    
+                    NombreDevie--;
+                }
+               
+            }
+            catch
+            {
+                Console.WriteLine("entrer un nombre svp");
+
+            }
+        }
+    }
+}
